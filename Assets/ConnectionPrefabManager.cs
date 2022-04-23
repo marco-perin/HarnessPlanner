bebed5125e47cb6d11f570385fd082698aa2893a
darkgap <mrpc98@gmail.com>
2022-04-23 17:05:06 +0200

    Fix build error

--- Assets/ConnectionPrefabManager.cs
+++ Assets/ConnectionPrefabManager.cs
@@ -60,17 +60,3 @@ public class ConnectionPrefabManager : MonoBehaviourGraphicInstanced
     }
 
 }
-
-[CustomEditor(typeof(ConnectionPrefabManager))]
-public class ConnectionPrefabManagerEditor : Editor
-{
-    public void OnSceneGUI()
-    {
-        var t = target as ConnectionPrefabManager;
-        if (t == null)
-            return;
-
-        t.From.position = Handles.DoPositionHandle(t.From.position, Quaternion.identity);
-        t.To.position = Handles.DoPositionHandle(t.To.position, Quaternion.identity);
-    }
-}
---
+++ Assets/Editor/ConnectionPrefabManagerEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ConnectionPrefabManager))]
+public class ConnectionPrefabManagerEditor : Editor
+{
+    public void OnSceneGUI()
+    {
+        var t = target as ConnectionPrefabManager;
+        if (t == null)
+            return;
+
+        t.From.position = Handles.DoPositionHandle(t.From.position, Quaternion.identity);
+        t.To.position = Handles.DoPositionHandle(t.To.position, Quaternion.identity);
+    }
+}
