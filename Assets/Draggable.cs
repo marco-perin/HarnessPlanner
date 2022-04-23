821d16f53857d7085cf2c8baab16663b2b3bd5dc
darkgap <mrpc98@gmail.com>
2022-04-23 03:08:42 +0200

    Cleanup Draggable.cs code

--- Assets/Draggable.cs
+++ Assets/Draggable.cs
@@ -1,76 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
-using Assets.GraphicData.Interfaces;
-//using Assets.UXData.Interfaces;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Draggable : MonoBehaviourGraphicInstanced, IDragHandler, IBeginDragHandler//, IDraggable
+public class Draggable : MonoBehaviourGraphicInstanced, IDragHandler, IBeginDragHandler
 {
     public float dragSpeed = 1;
-    Vector3 targetPosition;
 
-    public void StartInteraction(Vector3 param)
-    {
+    private Vector3 targetPosition;
+    private float screenScale = 0.01f;
 
-    }
-
-    //public void Drag(Vector3 dx)
-    //{
-
-    //    Debug.Log("Start Drag");
-    //    targetPosition += dx;
-    //}
-
-    // Start is called before the first frame update
     void Start()
     {
-
         targetPosition = GraphicInstance.Position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //if ((targetPosition - transform.position).sqrMagnitude > 1e-3)
-        //transform.position = Vector3.Slerp(transform.position, targetPosition, Time.deltaTime);
-        //errq = (targetPosition - GraphicInstance.Position).sqrMagnitude;
-        //GraphicInstance.Position = targetPosition;
         GraphicInstance.Position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * dragSpeed * 10);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-
-
-        Debug.Log("Dragging " + gameObject.name);
+        //Debug.Log("Dragging " + gameObject.name);
         if (eventData.pointerCurrentRaycast.isValid)
             targetPosition = (Vector2)eventData.pointerCurrentRaycast.worldPosition;
         else
-            targetPosition += (Vector3)eventData.delta * scale;
+            targetPosition += (Vector3)eventData.delta * screenScale;
     }
 
-    //public void OnPointerEnter(PointerEventData eventData)
-    //{
-    //    Debug.Log($"Entering OnPOinterenter on go {gameObject.name}");
-    //}
-
-    //private void OnDrawGizmos()
-    //{
-    //    Gizmos.DrawWireSphere(targetPosition, 5f * transform.localScale.x);
-    //}
-    //private float distance;
-    //private Plane dragPlane;
-
-    private float scale = 0.01f;
     public void OnBeginDrag(PointerEventData eventData)
     {
-        //distance = eventData.pointerCurrentRaycast.distance;
-        //var n = eventData.pointerCurrentRaycast.worldPosition - eventData.enterEventCamera.transform.position;
-        //dragPlane = new Plane(n, distance);
-        //scale = 0.01f;
-        scale = eventData.enterEventCamera.orthographicSize * 2 / eventData.enterEventCamera.pixelHeight;
-        //Debug.Log($"Scale = {eventData.enterEventCamera.orthographicSize * 2} / {eventData.enterEventCamera.pixelHeight}  = {scale}[pxpu]");
-        //Debug.Log($"Scale = {eventData.enterEventCamera.orthographicSize * 2} / {eventData.enterEventCamera.pixelWidth}  = {eventData.enterEventCamera.orthographicSize / eventData.enterEventCamera.pixelWidth}[pxpu]");
+        screenScale = eventData.enterEventCamera.orthographicSize * 2 / eventData.enterEventCamera.pixelHeight;
     }
 }
