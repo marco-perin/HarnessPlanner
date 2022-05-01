05d67ddefab602bc24dfb3b6209b1dd184d708d6
darkgap <mrpc98@gmail.com>
2022-05-01 19:28:52 +0200

    Update Addressable groups and small code cleanup

--- Assets/DataProcessPanelManager.cs
+++ Assets/DataProcessPanelManager.cs
@@ -7,40 +7,39 @@ using UnityEngine;
 
 public class DataProcessPanelManager : MonoBehaviour
 {
-    public TMPro.TMP_InputField inputField;
-    public TMPro.TMP_Dropdown inputDropDownField;
+   //public TMPro.TMP_InputField inputField;
+   public TMPro.TMP_Dropdown inputDropDownField;
 
-    private void Start()
-    {
-        inputField.text = MainCalculatorSingleton.Instance.BatteryNodeName;
-        inputDropDownField.onValueChanged.RemoveAllListeners();
-        inputDropDownField.onValueChanged.AddListener((value) => SetBatteryNameDirect(inputDropDownField.options[value].text));
-    }
+   private void Start()
+   {
+      //inputField.text = MainCalculatorSingleton.Instance.BatteryNodeName;
+      inputDropDownField.onValueChanged.RemoveAllListeners();
+      inputDropDownField.onValueChanged.AddListener((value) => SetBatteryNameDirect(inputDropDownField.options[value].text));
+   }
 
-    private void Update()
-    {
-        var sources = MainConnectionsManagerSingleton.Instance.connectionsParent
-                        .GetComponentsInChildren<GraphicalSOSync>()
-                        .Where(mbgi => mbgi.GraphicInstance is SourceGraphicBaseWrapper);
+   private void Update()
+   {
+      var sources = MainConnectionsManagerSingleton.Instance.connectionsParent
+                      .GetComponentsInChildren<GraphicalSOSync>()
+                      .Where(mbgi => mbgi.GraphicInstance is SourceGraphicBaseWrapper);
 
-        inputDropDownField.options = sources.Select(mbgi => new TMPro.TMP_Dropdown.OptionData((mbgi.GraphicInstance as SourceGraphicBaseWrapper).BaseWrapped.Name)).ToList();
+      inputDropDownField.options = sources.Select(mbgi => new TMPro.TMP_Dropdown.OptionData((mbgi.GraphicInstance as SourceGraphicBaseWrapper).BaseWrapped.Name)).ToList();
+
+   }
 
 
-    }
+   public void SetBatteryName()
+   {
+      //SetBatteryNameDirect(inputField.text);
+   }
 
+   public void SetBatteryNameDirect(string name)
+   {
+      MainCalculatorSingleton.Instance.BatteryNodeName = name;
+   }
 
-    public void SetBatteryName()
-    {
-        SetBatteryNameDirect(inputField.text);
-    }
-
-    public void SetBatteryNameDirect(string name)
-    {
-        MainCalculatorSingleton.Instance.BatteryNodeName = name;
-    }
-
-    public void ProcessData()
-    {
-        MainCalculatorSingleton.Instance.CalculateCurrents();
-    }
+   public void ProcessData()
+   {
+      MainCalculatorSingleton.Instance.CalculateCurrents();
+   }
 }
