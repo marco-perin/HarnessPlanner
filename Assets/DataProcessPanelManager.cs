d4fc19ac8cd09e4b8b1514ad8bee047dd8bcd7d2
darkgap <mrpc98@gmail.com>
2022-05-01 18:44:01 +0200

    Make Source Selector a dropdown

--- Assets/DataProcessPanelManager.cs
+++ Assets/DataProcessPanelManager.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Assets.CoreData.Interfaces;
+using Assets.GraphicData.Types;
 using UnityEngine;
 
 public class DataProcessPanelManager : MonoBehaviour
 {
     public TMPro.TMP_InputField inputField;
+    public TMPro.TMP_Dropdown inputDropDownField;
 
     private void Start()
     {
         inputField.text = MainCalculatorSingleton.Instance.BatteryNodeName;
+        inputDropDownField.onValueChanged.RemoveAllListeners();
+        inputDropDownField.onValueChanged.AddListener((value) => SetBatteryNameDirect(inputDropDownField.options[value].text));
     }
+
+    private void Update()
+    {
+        var sources = MainConnectionsManagerSingleton.Instance.connectionsParent
+                        .GetComponentsInChildren<GraphicalSOSync>()
+                        .Where(mbgi => mbgi.GraphicInstance is SourceGraphicBaseWrapper);
+
+        inputDropDownField.options = sources.Select(mbgi => new TMPro.TMP_Dropdown.OptionData((mbgi.GraphicInstance as SourceGraphicBaseWrapper).BaseWrapped.Name)).ToList();
+
+
+    }
+
 
     public void SetBatteryName()
     {
